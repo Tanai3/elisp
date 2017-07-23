953bdf8c29261ab4d8f9f1d6604d9f499adcdb31
chayka1206 <ec.chayka@gmail.com>
2017-07-23 15:04:22 +0900

    update

--- site-lisp/open-todo.el
+++ site-lisp/open-todo.el
@@ -1,8 +1,10 @@
 (defvar open-todo-file "~/Dropbox/TODO.org")
 
-(defun open-todo-start()
+(defun open-todo-start(bool)
   (interactive)
-  (find-file open-todo-file))
+  (if (and bool (not(file-exists-p open-todo-file)))
+      ()
+  (find-file open-todo-file)))
 
 (defun open-todo-set-filepath(path)
   (setq open-todo-file path))
